0e018af38561464bb04ef73e8e1e7e79af028622
Rafael Cabello <rafcabran@alum.us.es>
2023-05-14 17:27:52 +0200

    borrado codigo comentado

--- src/main/java/aiss/gitlabminer/controller/ProjectController.java
+++ src/main/java/aiss/gitlabminer/controller/ProjectController.java
@@ -3,31 +3,22 @@ package aiss.gitlabminer.controller;
 import aiss.gitlabminer.model.Commit;
 import aiss.gitlabminer.model.Issue;
 import aiss.gitlabminer.model.Project;
-import aiss.gitlabminer.repository.ProjectRepository;
-import aiss.gitlabminer.service.CommitService;
 import aiss.gitlabminer.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/gitlabminer")
 public class ProjectController {
-
-    private final ProjectRepository repository;
     @Autowired
     RestTemplate template;
     @Autowired
     ProjectService projectService;
-
-    public ProjectController(ProjectRepository repository) {
-        this.repository = repository;
-    }
 
     //POST http://localhost:8081/gitlabminer/{id}[?sinceCommits=5&sinceIssues=30&maxPages=2]
     @ResponseStatus(HttpStatus.CREATED)
@@ -43,12 +34,6 @@ public class ProjectController {
         String uri = "http://localhost:8080/gitminer/projects";
         Project createdProject = template.postForObject(uri, filtredProject, Project.class);
         return createdProject;
-
-        /*
-        Project _project = projectRepository.save(new Project(project.getId(), project.getName(), project.getWebUrl(),
-                project.getCommits(), project.getIssues()));
-
-         */
     }
 
     //GET http://localhost:8081/gitlabminer/{id}
@@ -61,21 +46,7 @@ public class ProjectController {
         List<Issue> issues = _project.getIssues().stream()
                 .filter(i -> LocalDateTime.parse(i.getUpdatedAt().substring(0,23)).isAfter(LocalDateTime.now().minusDays(sinceIssues))).toList();
         Project filtredProject = new Project(_project.getId(), _project.getName(), _project.getWebUrl(), commits, issues);
-        //String uri = "http://localhost:8080/gitminer/" + id.toString();
-        //Project createdProject = template.postForObject(uri, _project, Project.class);
         return filtredProject;
-    }
-
-    //GET http://localhost:8081/gitlabminer/projects
-    @GetMapping("/projects")
-    public List<Project> findAll() {
-        return repository.findAll();
-    }
-
-    //GET http://localhost:8081/gitlabminer/projects/{id}
-    @GetMapping("/projects/{id}")
-    public Project findOne(@PathVariable String id) {
-        return repository.findOne(id);
     }
 
 }
